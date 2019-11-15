005f6176567996fdc13e23c2a06fdab4ba2f717e
laskarks <laskar.ksatria12@gmail.com>
2019-11-15 08:52:48 +0700

    tambah-socket

--- app.js
+++ app.js
@@ -20,11 +20,74 @@ app.get('/', (req, res) => {
 })
 
 
+
+let count = 0;
+let enemyPosition = []
+
 io.on('connection', function (socket) {
   console.log('socket io now connect')
+
+
   socket.on('send', function (data) {
     io.emit('send', data)
   })
+
+
+  socket.on('playerId', (data) => {
+    count++
+    data.id = count
+    enemyPosition.push(data)
+    console.log(enemyPosition)
+    socket.emit('numberId', count)
+  })
+
+  socket.on('enemyPosition', data => {
+    socket.broadcast.emit('enemyPosition', data)
+  })
+
+  socket.on('getPlayerPosition', () => {
+    socket.emit('sendPlayerPosition', enemyPosition)
+  })
+  
+  socket.on('topPos', data => {
+    enemyPosition.forEach(function (enemy) {
+      if (enemy.id == data.id) {
+        enemy.top = data.top
+      }
+    })
+
+    socket.emit('getEnemy', enemyPosition)
+
+  })
+
+
+  socket.on('leftPos', data => {
+    enemyPosition.forEach(function (enemy) {
+      if (enemy.id == data.id) {
+        enemy.toLeft = data.toLeft
+      }
+    })
+    socket.emit('getEnemy', enemyPosition)
+  })
+
+  socket.on('rightPos', data => {
+    enemyPosition.forEach(function (enemy) {
+      if (enemy.id == data.id) {
+        enemy.toLeft = data.toLeft
+      }
+    })
+  })
+
+
+  socket.on('deg', data => {
+    enemyPosition.forEach(function (enemy) {
+      if (enemy.id == data.id) {
+        enemy.deg = data.deg
+      }
+    })
+    socket.emit('getEnemy', enemyPosition)
+  })
+
 })
 
 
